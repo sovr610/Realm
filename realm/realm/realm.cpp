2f174a9d6b9b322f8ca069bb9d489dcac1e41307
Parker Bidigare <pbidigare@cynerge.com>
2019-12-21 16:47:36 -0500

    a backup, not totally working, i know bad practice

--- realm/realm/realm.cpp
+++ realm/realm/realm.cpp
@@ -4,9 +4,56 @@
 #include <iostream>
 #include "include/SDL.h"
 #include <stdio.h>
-int main()
+
+const int SCREEN_WIDTH = 640;
+const int SCREEN_HEIGHT = 480;
+SDL_Window* window;
+SDL_Surface* screenSurface;
+
+
+
+int main(int argc, char* argv[])
 {
-    std::cout << "Hello World!\n";
+
+    //Initialize SDL
+    if (SDL_Init(SDL_INIT_VIDEO) < 0)
+    {
+        printf("SDL could not initialize! SDL_Error: %s\n", SDL_GetError());
+    }
+    else
+    {
+        //Create window
+        window = SDL_CreateWindow("SDL Tutorial", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN);
+        if (window == NULL)
+        {
+            printf("Window could not be created! SDL_Error: %s\n", SDL_GetError());
+        }
+        else
+        {
+            //Get window surface
+            screenSurface = SDL_GetWindowSurface(window);
+
+            //Fill the surface white
+            SDL_FillRect(screenSurface, NULL, SDL_MapRGB(screenSurface->format, 0xFF, 0xFF, 0xAA));
+
+            //Update the surface
+            SDL_UpdateWindowSurface(window);
+
+            //Wait two seconds
+            SDL_Delay(200000);
+        }
+    }
+
+    SDL_DestroyWindow(window);
+
+    //Quit SDL subsystems
+    SDL_Quit();
+    
+    std::cout << "hey";
+
+
+    return 0;
+    //std::cout << "Hello World!\n";
 }
 
 // Run program: Ctrl + F5 or Debug > Start Without Debugging menu
